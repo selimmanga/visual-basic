54590482f836531994f1b064db56fe32177e0094
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-10-23 16:05:38 +0300

    Create kronometre.vb

---
+++ kronometre.vb
@@ -0,0 +1,33 @@
+Dim dakika As Integer
+Dim saniye As Integer
+
+Private Sub basla_buton_Click()
+If Text1.Text = "" Then Text1.Text = 0
+If Text2.Text = "" Then Text2.Text = 0
+
+Timer1.Interval = 100
+Timer1.Enabled = True
+
+dakika = CInt(Text1.Text)
+saniye = CInt(Text2.Text)
+End Sub
+
+Private Sub Text2_Change()
+If Text2.Text > 60 Then Text2.Text = 60
+End Sub
+
+Private Sub Timer1_Timer()
+saniye = saniye - 1
+
+Text1.Text = dakika
+Text2.Text = saniye
+
+If saniye = 0 Then
+saniye = 59
+dakika = dakika - 1
+End If
+
+If Text1.Text = 0 And Text2.Text = 0 Then
+Timer1.Enabled = False
+End If
+End Sub
