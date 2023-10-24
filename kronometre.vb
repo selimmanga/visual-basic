76db98881b2ecda4f21214eafcf2028c4d0e4f99
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-10-24 20:17:19 +0300

    Update kronometre.vb

--- kronometre.vb
+++ kronometre.vb
@@ -12,22 +12,51 @@ dakika = CInt(Text1.Text)
 saniye = CInt(Text2.Text)
 End Sub
 
+Private Sub durdur_buton_Click()
+Timer1.Enabled = False
+End Sub
+
+Private Sub sifirla_buton_Click()
+Text1.Text = 0
+Text2.Text = 0
+Timer1.Enabled = False
+End Sub
+
 Private Sub Text2_Change()
 If Text2.Text > 60 Then Text2.Text = 60
 End Sub
 
 Private Sub Timer1_Timer()
-saniye = saniye - 1
 
-Text1.Text = dakika
-Text2.Text = saniye
+If gerisay.Value = True Then
 
-If saniye = 0 Then
-saniye = 59
-dakika = dakika - 1
+    saniye = saniye - 1
+    
+    Text1.Text = dakika
+    Text2.Text = saniye
+    
+    If saniye = 0 Then
+    saniye = 59
+    dakika = dakika - 1
+    End If
+    
+    If Text1.Text = 0 And Text2.Text = 0 Then
+    Timer1.Enabled = False
+    End If
+    
 End If
 
-If Text1.Text = 0 And Text2.Text = 0 Then
-Timer1.Enabled = False
+If ilerisay.Value = True Then
+
+    saniye = saniye + 1
+    
+    Text1.Text = dakika
+    Text2.Text = saniye
+    
+    If saniye = 60 Then
+    saniye = 0
+    dakika = dakika + 1
+    End If
+    
 End If
 End Sub
