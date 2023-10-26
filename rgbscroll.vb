46fd1cf114b6b0b921084403542182d07b1fd52c
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-10-26 09:26:09 +0300

    Update rgbscroll.vb
    Scroll ile belirlenen RGB değerlerini hexadecimal formatına dönüştürdük

--- rgbscroll.vb
+++ rgbscroll.vb
@@ -1,3 +1,9 @@
+Dim kirmiziHex As String
+
+Function RGBtoHex(ByVal r As Byte, ByVal g As Byte, ByVal b As Byte) As String
+    RGBtoHex = Right$("0" & Hex(r), 2) & Right$("0" & Hex(g), 2) & Right$("0" & Hex(b), 2)
+End Function
+
 Private Sub Form_Load()
 red.Max = 255
 red.Min = 0
@@ -7,6 +13,10 @@ green.Min = 0
 
 blue.Max = 255
 blue.Min = 0
+End Sub
+
+Private Sub donustur_Click()
+hexkodu.Text = "#" & RGBtoHex(red.Value, green.Value, blue.Value)
 End Sub
 
 Private Sub renk()
