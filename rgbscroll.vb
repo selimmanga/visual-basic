35d0010b5068a5a6c2bfa2ef3704f630899f2966
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-10-26 10:02:19 +0300

    Update rgbscroll.vb

--- rgbscroll.vb
+++ rgbscroll.vb
@@ -1,6 +1,6 @@
 'ByVal: Bu parametre ile oluşturulan değişken orijinalliğini korur ve üstünde işlem yapılamaz
 'Right$: Sağdan uzunluk alma işlemi 
-
+'Byte: yalnızca pozitif tam sayı değerlerini saklayabilir ve 8 bit uzunluğundadır
 Function RGBtoHex(ByVal r As Byte, ByVal g As Byte, ByVal b As Byte) As String
     RGBtoHex = Right$("0" & Hex(r), 2) & Right$("0" & Hex(g), 2) & Right$("0" & Hex(b), 2)
 End Function
