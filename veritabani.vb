9bca61304ddd0f6059f3938674abe676bfdf6a33
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-11-09 12:29:08 +0300

    Delete veritabani.vb

--- veritabani.vb
+++
@@ -1,7 +0,0 @@
-Private Sub kaydet_Click()
-Dim kaydet As New ADODB.Recordset
-
-'Çoklu kullanıcılar için adOpenStatic kullanıyoruz
-kaydet.Open "insert into tablo1(ad, soyad, yas) values ('" & ad.Text & "', '" & soyad.Text & "','" & yas.Text & "')", "dsn=masa", adOpenStatic
-aa = MsgBox("Kayıt başarılı", vbInformation, "Bilgi")
-End Sub
