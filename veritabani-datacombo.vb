5bd398d328ced0ad5fdb0c60315cb154eec9841d
Selim Manga <125385298+selimmanga@users.noreply.github.com>
2023-11-14 12:54:36 +0300

    Update veritabani-datacombo.vb

--- veritabani-datacombo.vb
+++ veritabani-datacombo.vb
@@ -1,25 +1,25 @@
 Private Sub hesapla_Click()
 Dim ortalama As New ADODB.Recordset
-Dim liste As New ADODB.Recordset
-Dim listele As New ADODB.Recordset
+Dim imlec_veriler As New ADODB.Recordset
+Dim imlec_liste As New ADODB.Recordset
 
+'Sorgular
 ortalama.Open "select avg(nott) as ort from tablo2", "dsn=masa", adOpenStatic
-liste.Open "select * from tablo2", "dsn=masa", adOpenStatic
-listele.Open "select distinct(bolum) from tablo2", "dsn=masa", adOpenStatic
+imlec_veriler.Open "select * from tablo2", "dsn=masa", adOpenStatic
+imlec_liste.Open "select distinct(bolum) from tablo2", "dsn=masa", adOpenStatic
 
-Label2.Caption = CInt(ortalama("ort")) 'Yukardaki sorguya "as ort" eklersek expr1000 yerine ort yazabiliriz
+ortalama_yazi.Caption = CInt(ortalama("ort")) 'Yukardaki sorguya "as ort" eklersek expr1000 yerine ort yazabiliriz
 
 With grid
-
-.Rows = liste.RecordCount + 1
+.Rows = imlec_veriler.RecordCount + 1
 .Cols = 4
 
-For t = 1 To liste.RecordCount
-.TextMatrix(t, 0) = liste("ad")
-.TextMatrix(t, 1) = liste("bolum")
-.TextMatrix(t, 2) = liste("nott")
+For t = 1 To imlec_veriler.RecordCount
+.TextMatrix(t, 0) = imlec_veriler("ad")
+.TextMatrix(t, 1) = imlec_veriler("bolum")
+.TextMatrix(t, 2) = imlec_veriler("nott")
 
-If CInt(liste("nott")) < CInt(ortalama("ort")) Then
+If CInt(imlec_veriler("nott")) < CInt(ortalama("ort")) Then
 .Row = t
 .Col = 2
 .CellBackColor = vbRed
@@ -31,9 +31,8 @@ Else
 .CellForeColor = vbWhite
 End If
 
-.TextMatrix(t, 3) = liste("cins")
-
-liste.MoveNext
+.TextMatrix(t, 3) = imlec_veriler("cins")
+imlec_veriler.MoveNext
 Next
 
 .TextMatrix(0, 0) = "Ad"
@@ -43,24 +42,26 @@ Next
 
 End With
 
-Set DataCombo1.RowSource = listele
+Set DataCombo1.RowSource = imlec_liste
 DataCombo1.ListField = "bolum"
 End Sub
 
 'Sadece listede seçilen bölümü grid'de yazdırır
 Private Sub DataCombo1_Click(Area As Integer)
-Dim a As New ADODB.Recordset
-a.Open "select * from tablo2 where bolum = '" & DataCombo1 & "'", "dsn=masa", adOpenStatic
+
+Dim imlec_bolum As New ADODB.Recordset
+imlec_bolum.Open "select * from tablo2 where bolum = '" & DataCombo1 & "'", "dsn=masa", adOpenStatic
 
 With grid
-.Rows = a.RecordCount + 1
+.Rows = imlec_bolum.RecordCount + 1
 .Cols = 4
-For t = 1 To a.RecordCount
-.TextMatrix(t, 0) = a("ad")
-.TextMatrix(t, 1) = a("bolum")
-.TextMatrix(t, 2) = a("nott")
-.TextMatrix(t, 3) = a("cins")
-a.MoveNext
+
+For t = 1 To imlec_bolum.RecordCount
+.TextMatrix(t, 0) = imlec_bolum("ad")
+.TextMatrix(t, 1) = imlec_bolum("bolum")
+.TextMatrix(t, 2) = imlec_bolum("nott")
+.TextMatrix(t, 3) = imlec_bolum("cins")
+imlec_bolum.MoveNext
 Next
 End With
 End Sub
